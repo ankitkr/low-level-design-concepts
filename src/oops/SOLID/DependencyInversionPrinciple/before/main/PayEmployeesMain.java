3665ab96b5ce65104f31c9f1b0a5c6f2646974f7
Ankit Kumar <ankit@paysense.in>
2020-07-10 23:26:55 +0530

    Refactor main class

--- src/oops/SOLID/DependencyInversionPrinciple/before/main/PayEmployeesMain.java
+++ src/oops/SOLID/DependencyInversionPrinciple/before/main/PayEmployeesMain.java
@@ -1,12 +1,26 @@
 package oops.SOLID.DependencyInversionPrinciple.before.main;
 
-import oops.SOLID.DependencyInversionPrinciple.before.payments.PaymentProcessor;
+import oops.SOLID.DependencyInversionPrinciple.before.employees.Employee;
+import oops.SOLID.DependencyInversionPrinciple.before.notification.EmployeeEmailSender;
+import oops.SOLID.DependencyInversionPrinciple.before.notification.Notifier;
+import oops.SOLID.DependencyInversionPrinciple.before.payments.EmployeePaymentProcessor;
+import oops.SOLID.DependencyInversionPrinciple.before.repositories.EmployeeFileRepository;
+import oops.SOLID.DependencyInversionPrinciple.before.repositories.Repository;
+import oops.SOLID.DependencyInversionPrinciple.before.serializer.EmployeeFileSerializer;
+
+
+import java.util.List;
 
 public class PayEmployeesMain {
 
     public static void main(String[] args) {
-        PaymentProcessor paymentProcessor = new PaymentProcessor();
-        int totalPayments = paymentProcessor.sendPayments();
+        Repository<Employee> employeeRepository =
+                new EmployeeFileRepository(new EmployeeFileSerializer());
+        List<Employee> employees = employeeRepository.findAll();
+        Notifier<Employee> notifier = new EmployeeEmailSender();
+
+        EmployeePaymentProcessor paymentProcessor = new EmployeePaymentProcessor(notifier);
+        int totalPayments = paymentProcessor.sendPayments(employees);
         System.out.println("Total payments " + totalPayments);
     }
 }
